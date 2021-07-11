17910f18f385f44df285ea9bd74ecd779b9a3f16
westsider <whansen1@mac.com>
2021-07-11 17:01:36 -0500

    Initial Commit

---
+++ AutoLayoutCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutCode
+//
+//  Created by Warren Hansen on 7/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
