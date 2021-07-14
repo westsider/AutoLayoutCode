94a814eb5b3c43c9ef3ca026a3ce244e8d253eea
westsider <whansen1@mac.com>
2021-07-14 09:41:45 -0500

    fixed layout issue

--- AutoLayoutCode/ViewController.swift
+++ AutoLayoutCode/ViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let liveImageView: UIImageView = {
-       let imageView = UIImageView(image: UIImage(named: "warren"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false 
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-        
+//    let parrentView: UIImageView = {
+//       let imageView = UIImageView(image: UIImage(named: "warren"))
+//        imageView.translatesAutoresizingMaskIntoConstraints = false
+//        imageView.contentMode = .scaleAspectFit
+//        return imageView
+//    }()
+//
     let instructText: UITextView = {
         let textView = UITextView()
         textView.text = " TURN HEAD LEFT"
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         textView.translatesAutoresizingMaskIntoConstraints = false
         return textView
     }()
-    
+
     let iconImageView: UIImageView = {
        let imageView = UIImageView(image: UIImage(named: "certente"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -37,89 +37,63 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    let parrentView = UIImageView(image: UIImage(named: "warren"))
+    
     let stackView   = UIStackView()
         
-    var myView = DrawView()
+    //var squareImage = UIImage()
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("imageView Bounds: \(liveImageView.bounds) view bounds: \(view.bounds)")
-            //= liveImageView.bounds.width * CGFloat( 0.75)
-    }
+    var childView = UIView();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         style()
         layout()
-        print("imageView Bounds: \(liveImageView.bounds) view bounds: \(view.bounds)")
-        //drawRectangle()
     }
     
-  
+    override func viewDidAppear(_ animated: Bool) {
+       
+        let width = parrentView.frame.width * 0.55
+        let height = parrentView.frame.height * 0.5
+
+        let frame = CGRect(x: 0, y: 0, width: width, height: height)
+        childView = DrawView(frame: frame)
+        childView.backgroundColor = UIColor.clear
+        childView.translatesAutoresizingMaskIntoConstraints = false
+        parrentView.addSubview(childView)
+        childView.center = CGPoint(x: parrentView.frame.size.width  / 2,
+                                     y: parrentView.frame.size.height / 2)
+    }
     
-
     func style() {
-
-        // image view size is not correct
-        print("imageView Bounds: \(liveImageView.bounds) view bounds: \(view.bounds)")
+        parrentView.translatesAutoresizingMaskIntoConstraints = false
+        parrentView.contentMode = .scaleAspectFit
         
-        // make crasshair sixw 75% of view
-        let crossWidth: CGFloat = view.bounds.width * 0.5
-        let crossHeight: CGFloat = crossWidth * 1.5 //  crossWidth * 1.5
-        
-        var x = liveImageView.bounds.minX  + ( crossWidth / 2 )
-        let y = liveImageView.bounds.minY  + ( crossHeight / 3 )
-        
-        myView = DrawView(frame: CGRect(x: x , y: y, width: crossWidth, height:crossHeight))
-        myView.backgroundColor = .clear
-        myView.translatesAutoresizingMaskIntoConstraints = false 
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis  = NSLayoutConstraint.Axis.vertical
         stackView.spacing   = 43.0
     }
     
-    func drawRectangle() {
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
 
-        let img = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
-
-            ctx.cgContext.setFillColor(UIColor.red.cgColor)
-            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
-            ctx.cgContext.setLineWidth(10)
-
-            ctx.cgContext.addRect(rectangle)
-            ctx.cgContext.drawPath(using: .fillStroke)
-        }
-
-        liveImageView.image = img
-    }
     
     func layout() {
         
-        
-        liveImageView.addSubview(myView)
-        
         //Stack View
-        stackView.addArrangedSubview(liveImageView)
+        stackView.addArrangedSubview(parrentView)
         stackView.addArrangedSubview(instructText)
         stackView.addArrangedSubview(iconImageView)
         self.view.addSubview(stackView)
-
-                
+  
         //Constraints
         NSLayoutConstraint.activate([
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            
         ])
-        
-        liveImageView.layoutIfNeeded()
-        myView.layoutIfNeeded()
-
-        
     }
 
 
